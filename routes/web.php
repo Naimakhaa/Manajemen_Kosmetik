5dfcd2ab0fea36aeb8b9db5888efee83ebb05dd0
kharizma <h1101241050@student.untan.ac.id>
2025-11-15 14:30:11 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Http\Controllers\MenuController;
+use Illuminate\Support\Facades\Route;
+
+// Login
+
+    // Menampilkan daftar menu
+    Route::get('/menu', [MenuController::class, 'index']);
+
+    // Menampilkan form tambah menu
+    Route::get('/tambahmenu', [MenuController::class, 'create']);
+
+    // Menyimpan menu baru
+    Route::post('/tambahmenu', [MenuController::class, 'store']);
+
+    // Menampilkan form edit menu
+    Route::get('/editmenu/{id}', [MenuController::class, 'edit']);
+
+    // Update data menu
+    Route::post('/editmenu/{id}', [MenuController::class, 'update']);
+
+    // Hapus data menu
+    Route::get('/hapusmenu/{id}', [MenuController::class, 'destroy']);
+
